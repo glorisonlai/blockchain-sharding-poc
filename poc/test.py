7bfe54fe84c76c3dc849afb56e067c45cb5514c6
Glorison <lai.glorison@gmail.com>
2021-09-13 21:34:36 +1000

    Removed poc folder

--- poc/test.py
+++
@@ -1,7 +0,0 @@
-from src.services import test_serial, test_shard
-
-if __name__ == '__main__':
-	transactions = 10 # Transactions generated and mined. Change this to make tests go faster
-	print(f'Serial POC: {int(test_serial(10) * 100) / 100}s')
-	print()
-	print(f'Sharding POC: {int(test_shard(10) * 100) / 100}s')
